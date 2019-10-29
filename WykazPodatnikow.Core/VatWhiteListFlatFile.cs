5553998e103b0fdf3d6dc987632fe38a5e5f8333
m4rcel <m4rcelpl@outlook.com>
2019-10-29 14:19:01 +0100

    Add basic support for Flat file to Core version

---
+++ WykazPodatnikow.Core/VatWhiteListFlatFile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using WykazPodatnikow.Data;
+using WykazPodatnikow.SharedLib;
+
+namespace WykazPodatnikow.Core
+{
+    public class VatWhiteListFlatFile
+    {
+        private JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        private readonly FlatFileData flatFileData;
+
+        public VatWhiteListFlatFile(string PathToJson)
+        {
+            if (!File.Exists(PathToJson))
+                throw new System.Exception("File not found");
+
+            try
+            {
+                flatFileData = JsonSerializer.Deserialize<FlatFileData>(File.ReadAllText(PathToJson), JsonSerializerOptions);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+
+            if(string.IsNullOrEmpty(flatFileData.head.datagenerowaniadanych))
+                throw new System.Exception("Invalide file, datagenerowaniadanych is empty");
+        }
+        
+        public bool IsInFlatFile(string nip, string bankAccount)
+        {
+            string hash = (flatFileData.head.datagenerowaniadanych + nip + bankAccount).SHA512();
+            foreach (var item in flatFileData.body)
+            {
+                if (item.Equals(hash))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
