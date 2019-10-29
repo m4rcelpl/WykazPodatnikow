74d4ac0835366ce7fb816958f0d8abb31e365258
m4rcel <m4rcelpl@outlook.com>
2019-10-29 12:11:45 +0100

    Wprowadzone zmiany z wersji API 1.3.0
    "Połączenie typów Person i EntityPerson jako EntityPerson"

--- WykazPodatnikow.Data/EntityPerson.cs
+++ WykazPodatnikow.Data/EntityPerson.cs
@@ -6,12 +6,36 @@ namespace WykazPodatnikow.Data
     ///
     /// </summary>
     [DataContract]
-    public class EntityPerson : Person
+    public class EntityPerson
     {
         /// <summary>
         /// Gets or Sets CompanyName
         /// </summary>
         [DataMember(Name = "companyName", EmitDefaultValue = false)]
         public string CompanyName { get; set; }
+
+        /// <summary>
+        /// Gets or Sets FirstName
+        /// </summary>
+        [DataMember(Name = "firstName", EmitDefaultValue = false)]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or Sets LastName
+        /// </summary>
+        [DataMember(Name = "lastName", EmitDefaultValue = false)]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Pesel
+        /// </summary>
+        [DataMember(Name = "pesel", EmitDefaultValue = false)]
+        public Pesel Pesel { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Nip
+        /// </summary>
+        [DataMember(Name = "nip", EmitDefaultValue = false)]
+        public string Nip { get; set; }
     }
 }
